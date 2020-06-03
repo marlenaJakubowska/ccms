698cd9bcd2147bf6414f86fdfc133e27a42fb3b1
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-06-03 11:22:41 +0200

    Change Dao structure - add UserDao interface

--- src/main/java/com/codecool/ccms/dao/Dao.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.ccms.dao;
-
-public interface Dao<T> {
-    void connect();
-    void add(String table, String[] columns, String[] values);
-    void remove(String table, String id);
-    void update(String table, String column, String newValue, String condition);
-
-}
