dffad037e3619ad3de35bac759087494c82bed5d
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-06-04 18:44:25 +0200

    Clean App, add dot to sql injection validation

--- src/main/java/com/codecool/ccms/App.java
+++ src/main/java/com/codecool/ccms/App.java
@@ -1,29 +1,12 @@
 package com.codecool.ccms;
 
 import com.codecool.ccms.controllers.LoginController;
-import com.codecool.ccms.controllers.ManagerMenuController;
-import com.codecool.ccms.controllers.MentorMenuController;
-import com.codecool.ccms.dao.UserDaoImpl;
-import com.codecool.ccms.models.User;
-import com.codecool.ccms.view.View;
 
 public class App
 {
     public static void main( String[] args )
     {
-        UserDaoImpl userDao = new UserDaoImpl();
-
-        //ManagerMenuController managerMenuController = new ManagerMenuController(userDao);
-        
-        //MentorMenuController mentorMenuController = new MentorMenuController(userDao);
-        //managerMenuController.displayAllUsers();
-        //managerMenuController.addMentor();
-        //managerMenuController.displayUserByRole();
-
-        //LoginController loginController = new LoginController();
-//        mentorMenuController.addStudent();
-//
-//        mentorMenuController.addStudent();
-
+        LoginController loginController = new LoginController();
+        loginController.init();
     }
 }
