6412b0f89e776bea4c08e289efdc630ef7fb2fbc
Marta Skarbek <marta@skarbek.name>
2020-06-04 10:52:29 +0200

    added ad student function

--- src/main/java/com/codecool/ccms/App.java
+++ src/main/java/com/codecool/ccms/App.java
@@ -1,6 +1,7 @@
 package com.codecool.ccms;
 
 import com.codecool.ccms.controllers.ManagerMenuController;
+import com.codecool.ccms.controllers.MentorMenuController;
 import com.codecool.ccms.dao.UserDaoImpl;
 import com.codecool.ccms.models.User;
 import com.codecool.ccms.view.View;
@@ -10,10 +11,13 @@ public class App
     public static void main( String[] args )
     {
         UserDaoImpl userDao = new UserDaoImpl();
-        ManagerMenuController managerMenuController = new ManagerMenuController(userDao);
-        managerMenuController.addMentor();
+        //ManagerMenuController managerMenuController = new ManagerMenuController(userDao);
+        MentorMenuController mentorMenuController = new MentorMenuController(userDao);
+        //managerMenuController.addMentor();
+        mentorMenuController.addStudent();
         for (User user : userDao.getAll()) {
             System.out.println(user.toString());
         }
+        mentorMenuController.addStudent();
     }
 }
